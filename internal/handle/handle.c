7b9257b470488cf304d17405b8ccdac9d937187d
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2026-08-09 21:29:51 +0800

    refactor: add internal/handle

---
+++ internal/handle/handle.c
@@ -0,0 +1,17 @@
+#include <windows.h>
+
+#include <handleapi.h>
+
+#include "moonbit.h"
+
+MOONBIT_FFI_EXPORT
+HANDLE
+moonbit_pty_internal_handle_win32_get_invalid_handle_value(void) {
+  return INVALID_HANDLE_VALUE;
+}
+
+MOONBIT_FFI_EXPORT
+HANDLE
+moonbit_pty_internal_handle_win32_get_null(void) {
+  return NULL;
+}
