fff5c970785b7d612d2e553b8546a51a5d5a2bef
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2026-08-10 00:40:01 +0800

    fix(internal/handle): remove NULL

--- internal/handle/handle.c
+++ internal/handle/handle.c
@@ -1,4 +1,5 @@
 #if defined(_WIN32)
+
 #include <windows.h>
 
 #include <handleapi.h>
@@ -10,9 +11,5 @@ HANDLE
 moonbit_pty_internal_handle_win32_get_invalid_handle_value(void) {
   return INVALID_HANDLE_VALUE;
 }
-
-MOONBIT_FFI_EXPORT
-HANDLE
-moonbit_pty_internal_handle_win32_get_null(void) { return NULL; }
 
 #endif
